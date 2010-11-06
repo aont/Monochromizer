ccd8fc40e5a1471968c54ba932ebbae54bf7e090
Naoto Aoki <aont50@gmail.com>
2010-11-06 22:02:43 +0900

    003

--- ImageForm.cs
+++ ImageForm.cs
@@ -26,11 +26,13 @@ namespace Aont
             this.openFileDialog1.ShowDialog();
         }
 
-        ConvertType convtype = ConvertType.Feedback;
         Bitmap Converted;
         private void ConvertClick(object sender, EventArgs e)
         {
+            if (this.Original == null)
+                return;
             Converted = new Bitmap(Original.Width, Original.Height, PixelFormat.Format1bppIndexed);
+
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
@@ -39,44 +41,6 @@ namespace Aont
             {
                 double alpha, beta;
 
-                if (convtype == ConvertType.Feedback)
-                {
-                    unsafe
-                    {
-                        double[,] brightness = new double[Width, Height];
-                        double b, b_ave = 0, b_sigma = 0;
-                        using (var proc_original = new BmpProc32(Original))
-                        {
-                            for (int y = 0; y < Height; ++y)
-                            {
-                                for (int x = 0; x < Width; ++x)
-                                {
-                                    b = brightness[x, y] = proc_original.GetPixel(x, y).GetBrightness();
-                                    b_ave += b;
-                                    b_sigma += b * b;
-                                }
-                            }
-                            b_ave /= Width * Height;
-                            b_sigma = Math.Sqrt(b_sigma / (Width * Height) - b_ave * b_ave);
-                        }
-
-                        for (int y = 0; y < Height; ++y)
-                        {
-                            for (int x = 0; x < Width; ++x)
-                            {
-                                b = brightness[x, y];
-                                alpha = rand.NextDouble();
-                                beta = rand.NextDouble();
-                                b += b_sigma * Math.Sqrt(-2 * Math.Log(alpha)) * Math.Cos(2 * Math.PI * beta);
-                                if (b > b_ave)
-                                    proc[x, y] = true;
-                                else
-                                    proc[x, y] = false;
-                            }
-                        }
-                    }
-                }
-                else if (convtype == ConvertType.Fixed)
                 {
                     double b;
                     using (var proc_original = new BmpProc32(Original))
@@ -87,8 +51,8 @@ namespace Aont
                                 b = proc_original[x, y].GetBrightness();
                                 alpha = rand.NextDouble();
                                 beta = rand.NextDouble();
-                                b += 0.1 * Math.Sqrt(-2 * Math.Log(alpha)) * Math.Cos(2 * Math.PI * beta);
-                                if (b > 0.5)
+                                b += this.b_sigma * Math.Sqrt(-2 * Math.Log(alpha)) * Math.Cos(2 * Math.PI * beta);
+                                if (b > this.b_average)
                                     proc[x, y] = true;
                                 else
                                     proc[x, y] = false;
@@ -105,7 +69,6 @@ namespace Aont
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-
             Original = new Bitmap(this.openFileDialog1.FileName);
 
             this.pictureBox1.Image = Original;
@@ -114,6 +77,8 @@ namespace Aont
 
         private void SaveClick(object sender, EventArgs e)
         {
+            if (this.Converted == null)
+                return;
             this.saveFileDialog1.ShowDialog();
         }
 
@@ -140,21 +105,57 @@ namespace Aont
             }
         }
 
-        private void feedbackToolStripMenuItem_Click(object sender, EventArgs e)
+        double b_average = 0.5, b_sigma = 0.1;
+        private unsafe void feedbackToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.convtype = ConvertType.Feedback;
+            double b;
+            using (var proc_original = new BmpProc32(Original))
+            {
+                for (int y = 0; y < Height; ++y)
+                {
+                    for (int x = 0; x < Width; ++x)
+                    {
+                        b = proc_original.GetPixel(x, y).GetBrightness();
+                        b_average += b;
+                        b_sigma += b * b;
+                    }
+                }
+                b_average /= Width * Height;
+                b_sigma = Math.Sqrt(b_sigma / (Width * Height) - b_average * b_average);
+            }
+
         }
 
-        private void fixedToolStripMenuItem_Click(object sender, EventArgs e)
+        private void brightnessToolStripMenuItem_DropDownOpening(object sender, EventArgs e)
         {
-            this.convtype = ConvertType.Fixed;
+
+            this.toolStripTextBox1.Text = b_average.ToString();
+            this.toolStripTextBox2.Text = b_sigma.ToString();
+        }
+
+
+        private void brightnessToolStripMenuItem_DropDownClosed(object sender, EventArgs e)
+        {
+            try
+            {
+                this.b_sigma = double.Parse(this.toolStripTextBox2.Text);
+                //this.averageToolStripMenuItem.HideDropDown();
+            }
+            catch { }
+            try
+            {
+                this.b_average = double.Parse(this.toolStripTextBox1.Text);
+                //this.averageToolStripMenuItem.HideDropDown();
+            }
+
+            catch { }
         }
 
 
     }
 
-    public enum ConvertType
-    {
-        Feedback, Fixed
-    }
+
+
 }
+
+
